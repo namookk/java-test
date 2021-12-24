5631db9a7c58cec736ca284a8f3dd00522862b69
namookk <papakang22@naver.com>
2021-12-24 16:23:20 +0900

    add : testContainer활용 docker-compose 활용 예제 추가

--- src/test/java/com/javatest/study/StudyServiceTest.java
+++ src/test/java/com/javatest/study/StudyServiceTest.java
@@ -4,24 +4,25 @@ import com.javatest.domain.Member;
 import com.javatest.domain.Study;
 import com.javatest.member.MemberService;
 import lombok.extern.slf4j.Slf4j;
-import net.bytebuddy.description.type.TypeList;
-import org.junit.After;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.commons.logging.Logger;
-import org.junit.platform.commons.logging.LoggerFactory;
 import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.io.File;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -35,23 +36,32 @@ import static org.mockito.Mockito.*;
 @ActiveProfiles("test")
 @Testcontainers
 @Slf4j
+//@ContextConfiguration(initializers = StudyServiceTest.ContainerPropertyInitializer.class)
 class StudyServiceTest {
 
     @Mock MemberService memberService;
     @Autowired StudyRepository studyRepository;
+    @Autowired Environment environment;
 
-    @Container
-    static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer()
-            .withDatabaseName("studytest");
+//    @Container
+//    static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer()
+//            .withDatabaseName("studytest");
 
 //    @Container
 //    static GenericContainer postgreSQLContainer = new GenericContainer("postgres")
 //            .withExposedPorts(5432) // 도커 안의 포트
-//            .withEnv("POSTGRES_DB", "studytest");
+//            .withEnv("POSTGRES_DB", "studytest")
+//            .withEnv("POSTGRES_PASSWORD", "studytest")
+//            .withEnv("POSTGRES_USERNAME", "studytest");
 
+    @Container
+    static DockerComposeContainer composeContainer =
+            new DockerComposeContainer(new File("src/test/resources/docker-compose.yml"));
 
     @BeforeEach
     void beforeEach(){
+        System.out.println("===============");
+        System.out.println(environment.getProperty("container.port"));
         studyRepository.deleteAll();
     }
 
@@ -59,12 +69,12 @@ class StudyServiceTest {
     static void beforeAll() {
         //Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(log);
         //postgreSQLContainer.followOutput(logConsumer);
-        postgreSQLContainer.start();
+        //postgreSQLContainer.start();
     }
 
     @AfterAll
     static void afterAll() {
-        postgreSQLContainer.stop();
+        //postgreSQLContainer.stop();
     }
 
     @Test
@@ -151,11 +161,13 @@ class StudyServiceTest {
 
     }
 
-
-
-
-
-
-
+    //spring 프로퍼티 설정
+    static class ContainerPropertyInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+//            TestPropertyValues.of("container.port="+postgreSQLContainer.getMappedPort(5432))
+//                    .applyTo(applicationContext.getEnvironment());
+        }
+    }
 
 }
