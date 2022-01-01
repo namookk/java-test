f815c487429aadf9d6850f197ca3fef1caf11856
namookk <papakang22@naver.com>
2022-01-01 16:25:02 +0900

    add : 성능테스트 방법 스크립트 예제 추가

--- scripts.sh
+++ scripts.sh
@@ -5,3 +5,11 @@ docker run -p 5432:5432 --name study-db -e POSTGRES_USER=study -e POSTGRES_PASSW
 #테스트용 DB
 docker run -p 15432:5432 --name study-testdb -e POSTGRES_USER=studytest -e POSTGRES_PASSWORD=studytest -e POSTGRES_DB=studytest -d postgres
 
+#httpie 사용 GET
+http http://localhost:8080/study/1
+
+#httpie 사용 POST
+http post http://localhost:8080/study limitCount=20 name=test
+
+#apach bench 사용
+ab -n 100 -c 10 http://localhost:8080/study/1
